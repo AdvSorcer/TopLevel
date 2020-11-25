0058d914be2bdfe63eb7051d373ae84770d06ab5
samsonhanku <hanku@samsonglobal.com>
2020-11-25 10:57:23 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+await GetUrlContentLengthAsync();
+Console.WriteLine("end");
+
+async Task<int> GetUrlContentLengthAsync()
+{
+    var client = new HttpClient();
+
+    Task<string> getStringTask =
+        client.GetStringAsync("https://docs.microsoft.com/dotnet");
+
+    Console.WriteLine("Working...");
+
+    string contents = await getStringTask;
+    Console.WriteLine(contents);
+
+    Console.WriteLine("hello");
+
+    return 2;
+}
+
